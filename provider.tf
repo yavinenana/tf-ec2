42f7a2f8169d63db9ae9afba93f32ed3f071cd17
jordypb <yavinenana>
2020-12-06 15:45:26 -0500

    [ADD] provider.tf and variables

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+        region = "${var.miregion}"
+        profile = "${var.miprofile}"
+}
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "miregion" {}
+variable "miprofile" {}
+
+#variable "ami_name" {}
+#variable "ami" {}
+#variable "owner_ami" {}
+#variable "type" {}
+#variable "key" {}
+#
+#variable "volsize" {}
+#variable "voltype" {}
+#variable "name" {}
+#variable "numero" {}
+#
+#variable "subnet" {}
+#variable "vpc_id" {}
+#
+#variable "service" {}
+#variable "versio" {}
+#variable "enviroment" {}
